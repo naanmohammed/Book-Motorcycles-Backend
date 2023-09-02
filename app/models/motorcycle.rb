fa15ab91e5f5d5a417a60647b11528ee9e499428
naanmohammed <naanahmed24@gmail.com>
2023-09-02 15:43:11 +0300

    Add model validations

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,4 +1,7 @@
 class Motorcycle < ApplicationRecord
   has_many :reservations
   has_many :users, through: :reservations
+
+  validates :brand, :model, :year, :image, presence: true
+  validates :rental_price, presence: true, numericality: { greater_than: 0 }
 end
