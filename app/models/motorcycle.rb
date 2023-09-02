a40a03ec6f4fa9bb54e409ac97f0f3bf170004c4
naanmohammed <naanahmed24@gmail.com>
2023-09-02 13:23:39 +0300

    Setup associations between models

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,2 +1,5 @@
 class Motorcycle < ApplicationRecord
+  belongs_to :user
+  has_many :reservations
+  has_many :users, through: :reservations
 end
--- db/migrate/20230902092635_create_users.rb
+++ db/migrate/20230902092635_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.boolean :admin, default: false
+      t.boolean :admin
 
       t.timestamps
     end
