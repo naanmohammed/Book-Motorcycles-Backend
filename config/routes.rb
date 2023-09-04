a176273774f28abd4effd31596c9376a72358a36
naanmohammed <naanahmed24@gmail.com>
2023-09-04 19:04:10 +0300

    Update configuration files

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :categories
-      resources :motorcycles
-      #resources :reservations
-      post '/signup', to: 'users#create'
-      post '/login', to: 'sessions#login'
-      get '/logout', to: 'sessions#destroy'
-      get '/authorized', to: 'sessions#logged_in?'
-      delete '/reservations/:id', to: 'reservations#destroy'
+      get '/users', to: 'users#index'
+      post "/signup", to: "users#create"
+      post "/login", to: "sessions#create"
+      get "/authorized", to: "sessions#show"
+
       get '/reservations', to: 'reservations#index'
       get '/reservations/:id', to: 'reservations#show'
       post '/add_reservation', to: 'reservations#create'
+      delete '/reservations/:id', to: 'reservations#destroy'
       get '/motorcycles', to: 'motorcycles#index'
       get '/motorcycles/:id', to: 'motorcycles#show'
       post '/add_motorcycle', to: 'motorcycles#create'
