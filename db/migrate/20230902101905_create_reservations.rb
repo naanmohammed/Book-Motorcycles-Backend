fef64ad5ef3b3e3d8729bd4eca217e0fa43f89c1
naanmohammed <naanahmed24@gmail.com>
2023-09-04 16:55:21 +0300

    Update user model

--- db/migrate/20230902101905_create_reservations.rb
+++ db/migrate/20230902101905_create_reservations.rb
@@ -4,6 +4,9 @@ class CreateReservations < ActiveRecord::Migration[7.0]
       t.references :motorcycle, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
       t.integer :total_price
+      t.date :start_date
+      t.date :end_date
+      t.string :city
 
       t.timestamps
     end
