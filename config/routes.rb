74c07a09f2082b59caed1573d00fe7b97fb661d4
naanmohammed <naanahmed24@gmail.com>
2023-09-04 17:22:34 +0300

    Update routes for reservations

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :categories
       resources :motorcycles
+      #resources :reservations
       post '/signup', to: 'users#create'
       post '/login', to: 'sessions#login'
       get '/logout', to: 'sessions#destroy'
       get '/authorized', to: 'sessions#logged_in?'
+      delete '/reservations/:id', to: 'reservations#destroy'
+      get '/reservations', to: 'reservations#index'
+      get '/reservations/:id', to: 'reservations#show'
+      post '/add_reservation', to: 'reservations#create'
     end
   end
 end
