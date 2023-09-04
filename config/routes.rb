68072e36e346d9fc19e21a83a4095ead4db04480
naanmohammed <naanahmed24@gmail.com>
2023-09-04 16:57:19 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   namespace :api do
     namespace :v1 do
+      resources :categories
       resources :motorcycles
+      post '/signup', to: 'users#create'
+      post '/login', to: 'sessions#login'
+      get '/logout', to: 'sessions#destroy'
+      get '/authorized', to: 'sessions#logged_in?'
     end
   end
-
 end
