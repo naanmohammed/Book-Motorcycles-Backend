fc465b21de2afdfe01ea851559cd46a571dcba2f
naanmohammed <naanahmed24@gmail.com>
2023-09-06 17:47:48 +0300

    Update migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,36 @@
 Rails.application.routes.draw do
+  root 'homes#index'
   namespace :api do
     namespace :v1 do
       get '/users', to: 'users#index'
       post "/signup", to: "users#create"
       post "/login", to: "sessions#create"
       get "/authorized", to: "sessions#show"
+      get "/logout", to: "sessions#logout"
 
       get '/reservations', to: 'reservations#index'
       get '/reservations/:id', to: 'reservations#show'
       post '/add_reservation', to: 'reservations#create'
-      delete '/reservations/:id', to: 'reservations#destroy'
-      get '/motorcycles', to: 'motorcycles#index'
-      get '/motorcycles/:id', to: 'motorcycles#show'
+      delete '/del_reservation', to: 'reservations#destroy'
+
+      get '/motorcycles', to: 'motorcycles#index' do
+        get :picture, on: :member
+      end
+      get '/motorcycles/:id', to: 'motorcycles#show' do
+        get :picture, on: :member
+      end
       post '/add_motorcycle', to: 'motorcycles#create'
-      delete '/motorcycles/:id', to: 'motorcycles#destroy'
+      delete '/del_motorcycle', to: 'motorcycles#destroy'
+      patch '/update_motorcycle', to: 'motorcycles#update'
+
+      get '/categories', to: 'categories#index' do
+        get :picture, on: :member
+      end
+      get '/categories/:id', to: 'categories#show' do
+        get :picture, on: :member
+      end
+      post '/add_category', to: 'categories#create'
+      delete '/del_category', to: 'categories#destroy'
     end
   end
 end
