b9f844213dfa142f6e5f5ff9d65582537e8e7e54
naanmohammed <naanahmed24@gmail.com>
2023-09-06 17:44:47 +0300

    Update serializers

--- app/serializers/motorcycle_serializer.rb
+++ app/serializers/motorcycle_serializer.rb
@@ -1,6 +1,10 @@
 class MotorcycleSerializer < ActiveModel::Serializer
-  attributes :id, :brand, :model, :year, :image, :rental_price
-
+  include Rails.application.routes.url_helpers
+  attributes :id, :brand, :model, :year, :image, :category_id, :rental_price, :reserved, :picture
   has_many :reservations
   has_many :users, through: :reservations
+
+  def picture
+    rails_blob_path(object.picture, only_path: true) if object.picture.attached?
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :admin, :email, :password
+  attributes :id, :name, :username, :admin, :email, :password
   has_many :reservations
   has_many :motorcycles, through: :reservations
 end
